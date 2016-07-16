6bd9818ca1002ea17736f62921f771e624998aa3
Joakim Soderberg <joakim.soderberg@netrounds.com>
2016-07-16 02:02:15 +0200

    Some more work

--- src/rest/main.go
+++ src/rest/main.go
@@ -1,7 +1,58 @@
 package main
 
-import "fmt"
+import (
+    "fmt"
+    "time"
+    "log"
+    "net/http"
+//  "strconv"
+
+    "github.com/emicklei/go-restful"
+    "github.com/emicklei/go-restful/swagger"
+)
+
+type CatEvent struct {
+    Id string
+    Start_time time.Time
+}
+
+type CatEventResource struct {
+    // TODO: Replace with MongoDB
+    events map[string]CatEvent
+}
+
+func (u CatEventResource) Register(container *restful.Container) {
+    ws := new(restful.WebService)
+
+    ws.Path("/events")
+      .Doc("Manage events")
+      .Consumes(restful.MIME_XML, restful.MIME_JSON)
+      .Produces()
+}
 
 func main() {
-	fmt.Println("hello world");
+    fmt.Println("hello worldsss");
+
+    wsContainer := restful.NewContainer()
+
+    cr := CatEventResource {map[string]CatEvent{}}
+
+    cr.Register(wsContainer)
+
+    // Swagger documentation.
+    // TODO: Set ports and all via command line options
+    config := swagger.Config {
+        WebServices:    wsContainer.RegisteredWebServices(), // you control what services are visible
+        WebServicesUrl: "http://localhost:8080",
+        ApiPath:        "/apidocs.json",
+
+        // Optionally, specifiy where the UI is located
+        SwaggerPath:     "/apidocs/",
+        SwaggerFilePath: "/swagger-ui-dist"}
+
+    swagger.RegisterSwaggerService(config, wsContainer)
+
+    log.Printf("start listening on localhost:8080")
+    server := &http.Server{Addr: ":8080", Handler: wsContainer}
+    log.Fatal(server.ListenAndServe())
 }
