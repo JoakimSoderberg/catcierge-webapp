63c656d165dc3a01c324020b77c77fe424523d05
Joakim Söderberg <joakim.soderberg@gmail.com>
2016-07-18 16:00:34 +0200

    Install swagger-ui via npm instead

--- src/rest/main.go
+++ src/rest/main.go
@@ -24,10 +24,29 @@ type CatEventResource struct {
 func (u CatEventResource) Register(container *restful.Container) {
     ws := new(restful.WebService)
 
-    ws.Path("/events")
-      .Doc("Manage events")
-      .Consumes(restful.MIME_XML, restful.MIME_JSON)
-      .Produces()
+    ws.Path("/events").
+        Doc("Manage events").
+        Consumes(restful.MIME_XML, restful.MIME_JSON).
+        Produces(restful.MIME_JSON, restful.MIME_XML)
+
+    ws.Route(ws.GET("/{event-id}").To(u.findEvent).
+        Doc("Get an event").
+        Operation("findEvent").
+        Param(ws.PathParameter("event-id", "identifier of the event").DataType("string")).
+        Writes(CatEvent{}))
+
+    container.Add(ws)
+}
+
+func (u CatEventResource) findEvent(request *restful.Request, response *restful.Response) {
+    id := request.PathParameter("event-id")
+    usr := u.events[id]
+    if len(usr.Id) == 0 {
+        response.AddHeader("Content-Type", "text/plain")
+        response.WriteErrorString(http.StatusNotFound, "404: Event could not be found.")
+        return
+    }
+    response.WriteEntity(usr)
 }
 
 func main() {
@@ -48,7 +67,7 @@ func main() {
 
         // Optionally, specifiy where the UI is located
         SwaggerPath:     "/apidocs/",
-        SwaggerFilePath: "/swagger-ui-dist"}
+        SwaggerFilePath: "/usr/local/lib/node_modules/swagger-ui/dist"}
 
     swagger.RegisterSwaggerService(config, wsContainer)
 
