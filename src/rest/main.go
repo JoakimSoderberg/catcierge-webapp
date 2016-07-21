67096ce0fa0a61f7b9b07d05296f3aedfa313ada
Joakim Söderberg <joakim.soderberg@gmail.com>
2016-07-21 17:00:02 +0200

    Add event.json compatible struct

--- src/rest/main.go
+++ src/rest/main.go
@@ -6,15 +6,78 @@ import (
     "log"
     "net/http"
 //  "strconv"
+//  "archive/zip" // TODO: Add unzip support
 
     "github.com/emicklei/go-restful"
     "github.com/emicklei/go-restful/swagger"
 )
 
 type CatEvent struct {
+    ID                  string `json:"id"`
+    EventJSONVersion    string `json:"event_json_version"`
+    CatciergeType       string `json:"catcierge_type"`
+    Description         string `json:"description"`
+    Start                time.Time `json:"start"`
+    End                 string `json:"end"`
+    TimeGenerated       time.Time `json:"time_generated"`
+    Timezone            string `json:"timezone"`
+    TimezoneUtcOffset   string `json:"timezone_utc_offset"`
+    GitHash             string `json:"git_hash"`
+    GitHashShort        string `json:"git_hash_short"`
+    GitTainted          int    `json:"git_tainted"`
+    MatchGroupCount     int    `json:"match_group_count"`
+    MatchGroupDirection string `json:"match_group_direction"`
+    MatchGroupMaxCount  int    `json:"match_group_max_count"`
+    MatchGroupSuccess   int    `json:"match_group_success"`
+    Matches             []struct {
+        Description string `json:"description"`
+        Directon    string `json:"direction"`
+        Filename    string `json:"filename"`
+        ID          string `json:"id"`
+        Path        string `json:"path"`
+        Result      int    `json:"result"`
+        StepCount   int    `json:"step_count"`
+        Steps       []struct {
+            Active      int    `json:"active"`
+            Description string `json:"description"`
+            Filename    string `json:"filename"`
+            Name        string `json:"name"`
+            Path        string `json:"path"`
+        } `json:"steps"`
+        Success int    `json:"success"`
+        Time    time.Time `json:"time"`
+    } `json:"matches"`
+    Rootpath  string `json:"rootpath"`
+    Settings  struct {
+        HaarMatcher struct {
+            Cascade       string `json:"cascade"`
+            EqHistogram   int    `json:"eq_histogram"`
+            InDirection   string `json:"in_direction"`
+            MinSizeHeight int    `json:"min_size_height"`
+            MinSizeWidth  int    `json:"min_size_width"`
+            NoMatchIsFail int    `json:"no_match_is_fail"`
+            PreyMethod    string `json:"prey_method"`
+            PreySteps     int    `json:"prey_steps"`
+        } `json:"haar_matcher"`
+        LockoutError      int    `json:"lockout_error"`
+        LockoutErrorDelay int    `json:"lockout_error_delay"`
+        LockoutMethod     int    `json:"lockout_method"`
+        LockoutTime       int    `json:"lockout_time"`
+        Matcher           string `json:"matcher"`
+        Matchtime         int    `json:"matchtime"`
+        NoFinalDecision   int    `json:"no_final_decision"`
+        OkMatchesNeeded   int    `json:"ok_matches_needed"`
+    } `json:"settings"`
+    State             string `json:"state"`
+    PrevState         string `json:"prev_state"`
+    Version           string `json:"version"`
+}
+
+/*
+type CatEvent struct {
     Id string
     Start_time time.Time
-}
+}*/
 
 type CatEventResource struct {
     // TODO: Replace with MongoDB
@@ -40,13 +103,13 @@ func (u CatEventResource) Register(container *restful.Container) {
 
 func (u CatEventResource) findEvent(request *restful.Request, response *restful.Response) {
     id := request.PathParameter("event-id")
-    usr := u.events[id]
-    if len(usr.Id) == 0 {
+    event := u.events[id]
+    if len(event.ID) == 0 {
         response.AddHeader("Content-Type", "text/plain")
         response.WriteErrorString(http.StatusNotFound, "404: Event could not be found.")
         return
     }
-    response.WriteEntity(usr)
+    response.WriteEntity(event)
 }
 
 func main() {
