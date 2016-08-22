dac4bb8be57d053651e1e4d2fbc2c0693398d19f
Joakim Soderberg <joakim.soderberg@gmail.com>
2016-08-22 21:20:36 +0200

    Move unzip to separate file

--- src/rest/main.go
+++ src/rest/main.go
@@ -1,17 +1,10 @@
 package main
 
 import (
-    //"fmt"
-    //"time"
     "log"
-    "net/http"
-    "archive/zip"
-    "os"
-    "io"
-    "path/filepath"
+    "net/http"    
     "github.com/emicklei/go-restful"
     "github.com/emicklei/go-restful/swagger"
-    //"labix.org/v2/mgo/bson"
 )
 
 type CatError struct {
@@ -47,65 +40,6 @@ func Returns404(b *restful.RouteBuilder) {
 
 func Returns500(b *restful.RouteBuilder) {
     b.Returns(http.StatusInternalServerError, http.StatusText(http.StatusInternalServerError), CatError{})
-}
-
-// TODO: Move to separate file
-func Unzip(src, dest string) error {
-    r, err := zip.OpenReader(src)
-    if err != nil {
-        return err
-    }
-    defer func() {
-        if err := r.Close(); err != nil {
-            panic(err)
-        }
-    }()
-
-    os.MkdirAll(dest, 0755)
-
-    // Closure to address file descriptors issue with all the deferred .Close() methods
-    extractAndWriteFile := func(f *zip.File) error {
-        rc, err := f.Open()
-        if err != nil {
-            return err
-        }
-        defer func() {
-            if err := rc.Close(); err != nil {
-                panic(err)
-            }
-        }()
-
-        path := filepath.Join(dest, f.Name)
-
-        if f.FileInfo().IsDir() {
-            os.MkdirAll(path, f.Mode())
-        } else {
-            f, err := os.OpenFile(path, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, f.Mode())
-            if err != nil {
-                return err
-            }
-            defer func() {
-                if err := f.Close(); err != nil {
-                    panic(err)
-                }
-            }()
-
-            _, err = io.Copy(f, rc)
-            if err != nil {
-                return err
-            }
-        }
-        return nil
-    }
-
-    for _, f := range r.File {
-        err := extractAndWriteFile(f)
-        if err != nil {
-            return err
-        }
-    }
-
-    return nil
 }
 
 func main() {
